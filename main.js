442911ca7d33d986f6a50872f37fb1afeabc8910
arthur <arthur.escriou@gmail.com>
2020-02-25 10:38:29 +0100

    :sparkles: print res

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 
-
 const readFiles = dirname => new Promise((resolve, reject) => fs.readdir(dirname, (err, filenames) => {
   if (err) {
     reject(err)
@@ -29,6 +28,7 @@ const readFile = path => {
   })
 
   return {
+    name: path.match(/data\/(.*?).txt/)[1],
     books: fLine[0],
     libraries: fLine[1],
     dayForScanning: fLine[2],
@@ -37,7 +37,39 @@ const readFile = path => {
   }
 }
 
+const score = (req, res) => {
+  return 0
+}
 
+const printResFile = (req, res) => {
+
+  const mapLibrary = library => {
+    const lLine = library.id.toString() + ' ' + library.books.length.toString()
+    const bLine = library.books.join(' ')
+    return lLine + '\n' + bLine
+  }
+
+  const fileToPrint = [
+      res.libraries.length.toString()
+    ].concat(res.libraries.map(mapLibrary))
+    .join('\n')
+  if (!res.hasOwnProperty('score')) {
+    res.score = score(req, res)
+  }
+  fs.writeFileSync('res/' + res.name + res.score, fileToPrint)
+}
+
+
+const compute = req => ({
+  name: 'a_example',
+  libraries: [{
+    id: 1,
+    books: [5, 2, 3]
+  }, {
+    id: 0,
+    books: [0, 1, 2, 3, 4]
+  }, ]
+})
 
 
 const main = async () => {
@@ -47,9 +79,10 @@ const main = async () => {
   // files.forEach(file => {
   //   readFile(folder + '/' + file)
   // })
-
-  console.log(readFile(folder + '/' + files[2]))
+  const file = readFile(folder + '/' + files[0])
+  console.log(file)
   console.log(files)
+  printResFile(file, compute(file))
 
 }
 
